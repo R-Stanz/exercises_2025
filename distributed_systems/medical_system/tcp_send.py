8ff41c84f6e8044b5905079f9be71bc03a99c8e3
R-Stanz <renatostanz@gmail.com>
2025-02-06 09:55:40 -0300

    Distributed Systems (Medical System) - TCP Send, Using BytesIO

--- distributed_systems/medical_system/tcp_send.py
+++ distributed_systems/medical_system/tcp_send.py
@@ -1,7 +1,7 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from Clinic_doctors import Clinic_doctors
 from Clinic import Clinic
-from os import remove, path
+from io import BytesIO
 
 clinic = Clinic("Health 2 Live", "8th St., MO", "232 432-5543")
 
@@ -9,26 +9,20 @@ clinic.add_doctor("Rafael Castro", "33432-KS", "02-25-1980", "orthopaedics")
 clinic.add_doctor("Seth White", "5432-NC", "09-10-1995", "psychiatry")
 clinic.add_doctor("Hannah Lontana", "12414325-NY", "07-04-2001", "general")
 
-
-tmp_file_path = 'tmp_clinic_doctors.bin'
-if not path.exists(tmp_file_path):
-    # Create the file if it does not exist
-    file = open(tmp_file_path, 'wb')
-    file.close()
-
-file = open(tmp_file_path, 'r+b')
-clinic_doctors_infos_stream = Clinic_doctors(clinic, file)
+message = BytesIO()
+clinic_doctors_infos_stream = Clinic_doctors(clinic, message)
 clinic_doctors_infos_stream.send_infos()
 
-file.seek(0)
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect(('127.0.0.1', 12345))
-data = file.read(1024)
+
+message.seek(0)
+data = message.read(1024)
+
 while data:
     s.send(data)
-    data = file.read(1024)
+    data = message.read(1024)
 
 s.close()
-file.close()
-remove(tmp_file_path)
+message.close()
